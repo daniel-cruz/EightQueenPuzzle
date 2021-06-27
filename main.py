2ccc992c1845c79da9facdba5d1658de5965b834
Daniel Cruz Gonzalez <daniel.cruzgonzalez.ext@bayer.com>
2021-06-27 14:37:54 -0500

    Solved point 1, and 2 without DB

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-#Test echo just for commit test
-echo "Hello world"
+from Classes.Queens import Queens
+import datetime
+
+print(datetime.datetime.now().strftime("%X"))
+for N in range(8,15):
+	Queens(N)
+	print(N)
+print(datetime.datetime.now().strftime("%X"))
